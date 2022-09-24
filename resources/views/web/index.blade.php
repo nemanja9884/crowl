a97a6b6bff95a6162a5fc845322376100da69956
Nemanja <nema3.84popovic@gmail.com>
2022-09-24 17:50:08 +0200

    - Started project

---
+++ resources/views/web/index.blade.php
@@ -0,0 +1,59 @@
+@extends('web.layouts.app')
+
+@section('content')
+    <main class="px-3">
+        <h1 class="mt-2">Please select your language</h1>
+        <div class="row">
+            <div class="col-md-4 mt-3">
+                <a href="#">
+                    <div class="card">
+                        <img src="{{asset('images/languages/flag-brazil-M.8a771559.jpg')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Brazil</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4 mt-3">
+                <a href="#">
+                    <div class="card">
+                        <img src="{{asset('images/languages/flag-serbia-M.71846a4b.jpg')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Serbia</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4 mt-3">
+                <a href="#">
+                    <div class="card">
+                        <img src="{{asset('images/languages/flag-estonia-M.32ee36a2.jpg')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Estonia</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4 mt-3">
+                <a href="#">
+                    <div class="card">
+                        <img src="{{asset('images/languages/flag-netherlands-M.13eb0383.jpg')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Netherlands</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4 mt-3">
+                <a href="#">
+                    <div class="card">
+                        <img src="{{asset('images/languages/flag-slovenia-M.11c1a804.jpg')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Slovenia</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </main>
+@endsection
---
+++ resources/views/web/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body class="d-flex h-100 text-center text-white bg-dark">
+
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="mb-auto">
+        <div>
+            <h3 class="float-md-start mb-0">Cover</h3>
+            <nav class="nav nav-masthead justify-content-center float-md-end">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Contact</a>
+            </nav>
+        </div>
+    </header>
+
+    @yield('content')
+
+    <footer class="mt-auto text-white-50">
+       Copyright © {{\Carbon\Carbon::now()->year}} {{ config('app.name', 'Laravel') }}
+    </footer>
+</div>
+</body>
+</html>
