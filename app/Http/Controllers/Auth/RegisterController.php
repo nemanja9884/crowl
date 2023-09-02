d57ef985ded9236983e4d2f976c0bc940e73cd4f
Nemanja <nema3.84popovic@gmail.com>
2023-09-03 01:37:03 +0200

    - Translation finished

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -55,7 +55,9 @@ class RegisterController extends Controller
             'username' => ['required', 'string', 'max:255', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ],['']);
+        ],[
+            'username.unique' => 'That username is already taken. Please select another.',
+        ]);
     }
 
     /**
