c679beb4bbb586bb7d6a2d9caa1dab184089a026
Nemanja <nema3.84popovic@gmail.com>
2023-09-20 20:04:47 +0200

    - Translation finished

--- resources/views/web/badges.blade.php
+++ resources/views/web/badges.blade.php
@@ -1,6 +1,6 @@
 @foreach($badges as $badge)
     <div class="d-flex" style="align-items: center;">
-        <img src="{{$badge->image}}" style="max-width: 50px; margin-right: 10px;" /> <h5>{{trans('home.Badge') . " " . $loop->iteration}} {{$badge->name}}</h5>
+        <img src="{{$badge->image}}" style="max-width: 50px; margin-right: 10px;" /> <h5>{{trans('home.Badge') . " " . $loop->iteration}} {{trans('home.' . $badge->name)}}</h5>
     </div>
     <p>- {{trans('home.Requirements: Reach')}} {{$badge->points}} {{trans('home.points')}}</p>
     <p>- {{trans('home.Description:')}} {{trans('home.' . $badge->description)}}</p>
